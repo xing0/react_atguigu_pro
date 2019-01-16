505a46043eb3224f8a20db293ef389eb6da92bb4
Xing <xing@xing.com>
2019-01-16 11:58:33 +0800

    完美修复后,准备做admin布局

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {Route,Switch,BrowserRouter} from 'react-router-dom'
+import Admin  from './page/admin/admin'
+import Login from './page/login/login'
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login' component={Login}  />
+                    <Route path='/' component={Admin}  />
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
